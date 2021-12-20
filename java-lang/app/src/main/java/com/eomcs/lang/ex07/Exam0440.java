ea120053f75ad111c727b5ac11fea0dbbd6c975f
jys7622 <jys7622@gmail.com>
2021-12-20 22:32:01 +0900

    ,

--- java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0440.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0440.java
@@ -5,8 +5,10 @@ package com.eomcs.lang.ex07;
 public class Exam0440 {
 
   static int m1(int value) {
-    int r1 = m2(value);
-    int r2 = m3(value);
+    int r1 = m2(value);  
+// m1메서드 호출시 JVM Stack에 로컬변수 생성, 변수 자체를 직접 r1에 넣는 것이 아니라 변수 안에 있는 값 value를 넣는것
+    int r2 = m3(value);  
+// m1메서드 호출시 JVM Stack에 로컬변수 생성 
     return r1 + r2;
   }
 
@@ -26,7 +28,7 @@ public class Exam0440 {
 // JVM Stack 메모리의 사용
 // 0) 시작
 // 1) main()
-// 2) main() => m1()
+// 2) main() => m1() - 함수의 파라미터도 로컬변수이기 때문에 args도 로컬변수.
 // 3) main() => m1() => m2()
 // 4) main() => m1()
 // 5) main() => m1() => m3()
